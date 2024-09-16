7cc13b9ff4a8f31bf3da0b9c74f8ef5c4f47600a
Muhammad Faqih <freemiumlemper300@gmail.com>
2024-09-16 19:56:55 +0700

    Creating movie list and search with API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useEffect } from "react";
+
+import './App.css'
+import SearchIcon from './Search.svg'
+
+const API_URL = 'http://www.omdbapi.com/?apikey=5f6d82ca'
+
+const movieEx = {
+    "Title": "Minecraft",
+    "Year": "2009",
+    "imdbID": "tt2011970",
+    "Type": "game",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZTc4ZTQ2YTctZjllYS00ZjUwLWI3NDktYzU5NmU5MTY3ODZjXkEyXkFqcGdeQXVyNjgzMzA5Mzk@._V1_SX300.jpg"
+}
+
+const App = () => {
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovie('Minecraft')
+    })
+    return (
+        <div className="app">
+            <h1>MovieList</h1>
+            <div className="search">
+                <input
+                    placeholder="Cari film disini"
+                    value="Minecraft"
+                    onChange={() => { }} />
+                <img src={SearchIcon}
+                    alt="search"
+                    onClick={() => { }} />
+            </div>
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movieEx.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movieEx.Poster !== 'N/A' ? movieEx.Poster : 'https://placehold.co/400'} alt={movieEx.Title} />
+                    </div>
+                    <div>
+                        <span>{movieEx.Type}</span>
+                        <h3>{movieEx.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
